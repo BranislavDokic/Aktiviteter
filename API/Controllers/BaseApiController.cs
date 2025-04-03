d79201b82273425838281d16849ab4c3344608e5
BranislavDokic <143610751+BranislavDokic@users.noreply.github.com>
2025-04-03 16:17:32 +0200

    finsh with implementing from for map and others...

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,6 +1,8 @@
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace API.Controllers
 {
@@ -10,8 +12,17 @@ namespace API.Controllers
     {
         private IMediator? _mediator;
 
-        protected IMediator Mediator => 
+        protected IMediator Mediator =>
            _mediator ??= HttpContext.RequestServices.GetService<IMediator>()
            ?? throw new InvalidOperationException("IMediator service is unvalible");
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (!result.IsSuccess && result.Code == 404) return NotFound();
+
+            if (result.IsSuccess && result.Value != null) return Ok(result.Value);
+
+            return BadRequest(result.Error);
+        }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,8 @@
+using API.Middleware;
 using Application.Activities.queries;
+using Application.Activities.Validators;
 using Application.Core;
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -15,12 +18,22 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 }
 );
 builder.Services.AddCors();
-builder.Services.AddMediatR(x =>
-     x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+builder.Services.AddMediatR(x => {
+     x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>();
+     x.AddOpenBehavior(typeof(ValidationBehacier<,>));
+     
+});
+
+
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+builder.Services.AddValidatorsFromAssemblyContaining<CreateActivityValidator>();
+builder.Services.AddTransient<ExceptionMiddleware>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
