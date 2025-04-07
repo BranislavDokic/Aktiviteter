5207ba8009f9bd6ac1e67ac76b1a83578a27bab9
BranislavDokic <143610751+BranislavDokic@users.noreply.github.com>
2025-04-07 14:15:29 +0200

    finish implementing api image upload

---
+++ Domain/Photo.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
+
+namespace Domain;
+
+public class Photo
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public required string Url { get; set; }
+
+    public required string PublicId { get; set; }
+
+
+
+    public required string UserId { get; set; }
+
+    [JsonIgnore]
+    public User User { get; set; } = null!;
+}
