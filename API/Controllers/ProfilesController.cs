2932bde3130a917222adaf9f73d50531b44e7f7f
BranislavDokic <143610751+BranislavDokic@users.noreply.github.com>
2025-04-08 14:21:22 +0200

    finis implementing editor for editing bio of user

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -1,41 +1,47 @@
 using System;
- using Application.Profiles.Commands;
- using Application.Profiles.DTOs;
- using Application.Profiles.Queries;
- using Domain;
- using Microsoft.AspNetCore.Mvc;
- 
- namespace API.Controllers;
- 
- public class ProfilesController : BaseApiController
- {
-     [HttpPost("add-photo")]
-     public async Task<ActionResult<Photo>> AddPhoto(IFormFile file)
+using Application.Profiles.Commands;
+using Application.Profiles.DTOs;
+using Application.Profiles.Queries;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class ProfilesController : BaseApiController
+{
+    [HttpPost("add-photo")]
+    public async Task<ActionResult<Photo>> AddPhoto(IFormFile file)
+    {
+        return HandleResult(await Mediator.Send(new AddPhoto.Command { File = file }));
+    }
+
+    [HttpGet("{userId}/photos")]
+    public async Task<ActionResult<List<Photo>>> GetPhotosForUser(string userId)
+    {
+        return HandleResult(await Mediator.Send(new GetProfilePhotos.Query { UserId = userId }));
+    }
+
+    [HttpDelete("{photoId}/photos")]
+    public async Task<ActionResult> DeletePhoto(string photoId)
+    {
+        return HandleResult(await Mediator.Send(new DeletePhoto.Command { PhotoId = photoId }));
+    }
+
+    [HttpPut("{photoId}/setMain")]
+    public async Task<ActionResult> SetMainPhoto(string photoId)
+    {
+        return HandleResult(await Mediator.Send(new SetMainPhoto.Command { PhotoId = photoId }));
+    }
+
+    [HttpGet("{userId}")]
+    public async Task<ActionResult<UserProfile>> GetProfile(string userId)
+    {
+        return HandleResult(await Mediator.Send(new GetProfile.Query { UserId = userId }));
+    }
+
+    [HttpPut]
+     public async Task<ActionResult> UpdateProfile(EditProfile.Command command)
      {
-         return HandleResult(await Mediator.Send(new AddPhoto.Command{File = file}));
+         return HandleResult(await Mediator.Send(command));
      }
- 
-     [HttpGet("{userId}/photos")]
-     public async Task<ActionResult<List<Photo>>> GetPhotosForUser(string userId)
-     {
-         return HandleResult(await Mediator.Send(new GetProfilePhotos.Query{UserId = userId}));
-     }
- 
-     [HttpDelete("{photoId}/photos")]
-     public async Task<ActionResult> DeletePhoto(string photoId)
-     {
-         return HandleResult(await Mediator.Send(new DeletePhoto.Command{PhotoId = photoId}));
-     }
- 
-     [HttpPut("{photoId}/setMain")]
-     public async Task<ActionResult> SetMainPhoto(string photoId)
-     {
-         return HandleResult(await Mediator.Send(new SetMainPhoto.Command{PhotoId = photoId}));
-     }
- 
-     [HttpGet("{userId}")]
-     public async Task<ActionResult<UserProfile>> GetProfile(string userId)
-     {
-         return HandleResult(await Mediator.Send(new GetProfile.Query{UserId = userId}));
-     }
- }
+}
